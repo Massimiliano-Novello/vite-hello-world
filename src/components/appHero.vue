a09ac09075b1537f1ad48a0103b828076c45e047
Massimiliano-Novello <massinov5@gmail.com>
2023-04-12 15:44:31 +0200

    Esercizio con vite

---
+++ src/components/appHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "appHero",
+    data () {
+        return{
+            jumbotron: "Jumbotron vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h3>{{ jumbotron }}</h3>
+</template>
+
+<style>
+h3 {
+    color: red;
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
---
+++ src/components/appTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "appTitle",
+    data () {
+        return{
+            title: "La mia prima app con vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style>
+h1 {
+    color: blue;
+    font-size: 3rem;
+    text-align: center;
+    margin-top: 10px;
+}
+</style>
